64a9c84d695f5f9179a568ce7ee924cca0b6beb0
kazui hitoshi <kazuihitoshi@gmail.com>
2022-05-19 23:29:05 +0900

    commit

--- markdownparse.js
+++ markdownparse.js
@@ -1,7 +1,8 @@
 var mdEls = document.querySelectorAll('.markdown');
 for(var i=0; i< mdEls.length ;i++){
-    var mdEl = mdEls[i];
-    var converted = document.createElement('div');
-    converted.innerHTML = marked.parse(mdEl.value);
-    mdEl.parentNode.replaceChild(converted, mdEl);
+    //var mdEl = mdEls[i];
+    mdEls[i].innerHTML = marked.parse(mdEls[i].innerHTML);
+    //var converted = document.createElement('div');
+    //converted.innerHTML = marked.parse(mdEl.value);
+    //mdEl.parentNode.replaceChild(converted, mdEl);
 }
